aa04bdbd66ea97407a22aac5a9dbf56de164e4da
Subash Pradahan <suprowilde5@gmail.com>
2019-09-04 00:32:07 +0200

    Router for homepage and vuetify is working

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from '@/plugins/vuetify'
+import vuetify from './plugins/vuetify'
 import router from './router'
-import 'vuetify/dist/vuetify.min.css'
-import LatestMovie from './components/LatestMovie'
 
 Vue.use(vuetify)
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  components : {App, 'LatestMovie': LatestMovie},
+  // components : {App, 'LatestMovie': LatestMovie},
   render: h => h(App),
   router
 }).$mount('#app')
