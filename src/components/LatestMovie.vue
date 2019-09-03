abf4e27fcf73af9f903196ff9cc887054bb4ca1b
Subash Pradahan <suprowilde5@gmail.com>
2019-09-03 12:36:37 +0200

    Fetch data from omdbapi working

---
+++ src/components/LatestMovie.vue
@@ -0,0 +1,27 @@
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'LatestMovie',
+  data () {
+    return {
+      wholeResponse: [],
+      loading: true
+    }
+  },
+  mounted () {
+  axios
+    .get('http://www.omdbapi.com/?s=horror&apikey=d03e9c79&page=1&type=movie&Content-Type=application/json')
+    .then(response => {
+      this.wholeResponse = response.data.Search
+      this.loading = false
+      console.log("DATA", this.wholeResponse)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
