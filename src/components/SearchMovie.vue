94505dce39be6e4281bc29a2d14599b36bd9a93f
Subash Pradahan <suprowilde5@gmail.com>
2019-09-04 23:07:29 +0200

    Search movie with list ready and little css

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -6,48 +6,59 @@
   </v-container>
 
   <v-container v-else grid-list-xl>
+    <h2>Your Movies</h2>
     <v-layout wrap>
-      <v-card>
-        <v-img :src="searchMovie.Poster" aspect-ratio="2.5" contain></v-img>
-        <v-card-title primary-title class="justify-center">
-          <div class="description">
-            <h3>{{searchMovie.Title}}</h3>
-            <div>Year: {{searchMovie.Year}}</div>
-            <div>Rated: {{searchMovie.Rated}}</div>
-            <div>IMDB-Rating: {{searchMovie.imdbRating}}</div>
-            <div>Actors: {{searchMovie.Actors}}</div>
-            <div>Description: {{searchMovie.Plot}}</div>
+      <v-flex xs3 v-for="(selectedMovie, index) in searchMovie" :key="index" mt-10 ml-12>
+        <v-card>
+          <v-img :src="selectedMovie.Poster" alt="item.Title" aspect-ratio="1" />
+          <v-card-title primary-title hover>
+            <h4>{{selectedMovie.Title}}</h4>
+          </v-card-title>
+          <div class="movie-text">
+            <div>Year: {{selectedMovie.Year}}</div>
+            <div>Type: {{selectedMovie.Type}}</div>
           </div>
-        </v-card-title>
-        <v-card-actions class="justify-center">
-          <v-btn color="blue" @click="back">Back to Homepage</v-btn>
-        </v-card-actions>
-      </v-card>
+          <v-card-actions class="justify-center">
+            <v-btn color="blue" @click="singleMovie(selectedMovie.imdbID)">Details</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
+
+    <v-card-actions class="justify-center">
+      <v-btn color="red" @click="back">Movie magic</v-btn>
+    </v-card-actions>
+
+    <v-footer class="text-center footer" app dark>
+      <strong>
+        Subash Pradhan
+        {{ new Date().getFullYear() }}
+        @Codaisseur
+      </strong>
+    </v-footer>
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
-import LatestMovie from './LatestMovie'
+import LatestMovie from "./LatestMovie";
 export default {
-  // name: 'LatestMovie',
-  // components :{LatestMovie},
-  props : ['searchText'],
+  props: ["searchText"],
   data() {
     return {
-      text:'',
-      searchMovie: '',
+      text: "",
+      searchMovie: [],
       loading: true
     };
   },
   mounted() {
     axios
       .get(
-        `http://www.omdbapi.com/?apikey=d03e9c79&t=${this.searchText}`
+        // `http://www.omdbapi.com/?apikey=d03e9c79&t=${this.searchText}`
+        `http://www.omdbapi.com/?s=${this.searchText}&apikey=d03e9c79&page=1&type=movie&Content-Type=application/json`
       )
       .then(response => {
-        this.searchMovie = response.data;
+        this.searchMovie = response.data.Search;
         this.loading = false;
       })
       .catch(error => {
@@ -55,6 +66,10 @@ export default {
       });
   },
   methods: {
+    singleMovie(id) {
+      this.$router.push("/movie/" + id);
+    },
+
     back() {
       this.$router.push("/");
     }
@@ -66,8 +81,33 @@ export default {
 .v-progress-circular {
   margin: 1rem;
 }
+
 .description {
   text-align: center;
-  font-family: 'Anton', sans-serif;
+  font-family: "Anton", sans-serif;
+}
+
+.container {
+  background-image: url("../image/giphy.gif");
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+}
+
+h2 {
+  color: red;
+  text-align: center;
+  font-family: "Kaushan Script", cursive;
+  font-size: 40px;
+}
+
+.movie-text,
+h4 {
+  text-align: center;
+  font-family: "Anton", sans-serif;
+}
+
+.footer {
+  justify-content: center;
 }
 </style>
